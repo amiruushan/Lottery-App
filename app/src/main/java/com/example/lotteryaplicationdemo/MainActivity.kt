ea366d803f5a5bc69a814b503bd5c95de04b8a85
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-02-16 14:19:40 +0530

    question 2 - new buttons for each value

--- app/src/main/java/com/example/lotteryaplicationdemo/MainActivity.kt
+++ app/src/main/java/com/example/lotteryaplicationdemo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.lotteryaplicationdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,7 +41,7 @@ fun GUI(){
     var num4: String by remember{ mutableStateOf("")}
     var num5: String by remember{ mutableStateOf("")}
     var num6: String by remember{ mutableStateOf("")}
-    var numbers = calculate().toMutableList()
+    var numbers: ArrayList<Int> by remember{ mutableStateOf(ArrayList())}
 
     Column(
         Modifier.fillMaxSize(),
@@ -62,7 +60,7 @@ fun GUI(){
         }
 
         Button(onClick = {
-            numbers = calculate().toMutableList()
+            numbers = calculate()
             num1 = numbers[0].toString()
             num2 = numbers[1].toString()
             num3 = numbers[2].toString()
@@ -74,26 +72,91 @@ fun GUI(){
             Text("Generate")
         }
 
+        // Button for val 1
+        Button(onClick = {
+            var changeNum = changeOneValue(0, numbers)
+            num1 = numbers[0].toString()
+        })
+        {
+            Text("1")
+        }
+
+        // Button for val 2
+        Button(onClick = {
+            var changeNum = changeOneValue(1, numbers)
+            num2 = numbers[1].toString()
+        })
+        {
+            Text("2")
+        }
+
+        // Button for val 3
+        Button(onClick = {
+            var changeNum = changeOneValue(2, numbers)
+            num3 = numbers[2].toString()
+        })
+        {
+            Text("3")
+        }
+
+        // Button for val 4
+        Button(onClick = {
+            var changeNum = changeOneValue(3, numbers)
+            num4 = numbers[3].toString()
+        })
+        {
+            Text("4")
+        }
+
+        // Button for val 5
+        Button(onClick = {
+            var changeNum = changeOneValue(4, numbers)
+            num5 = numbers[4].toString()
+        })
+        {
+            Text("5")
+        }
+
+        // Button for val 6
+        Button(onClick = {
+            var changeNum = changeOneValue(5, numbers)
+            num6 = numbers[5].toString()
+        })
+        {
+            Text("6")
+        }
+
+
+
     }
 
 }
 
 
-fun calculate():List<Int>{
-    var numbers : MutableList<Int> = mutableListOf()
+fun calculate():ArrayList<Int>{
+    var numbers : ArrayList<Int> = ArrayList()
 
-    while(numbers.size<6){
+    while(numbers.size < 6){
         var num = Random.nextInt(59)+1
         if (num !in numbers)
             numbers.add(num)
     }
 
-    var res = ""
-    for(i in numbers){
-        res += "" + i + " "
-    }
 
     println("Entered calculate()");
     return numbers
 }
+
+fun changeOneValue(index:Int, numbers: ArrayList<Int>): ArrayList<Int> {
+    var newVal: Int = Random.nextInt(59)+1
+    for (num in numbers){
+        if (newVal == num){
+            newVal = Random.nextInt(59)+1
+        }
+    }
+    numbers[index] = newVal
+
+    return numbers
+}
+
 
